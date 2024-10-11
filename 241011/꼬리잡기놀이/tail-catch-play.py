37733546f6a96d5d70460dd787194829168f9f69
dangeunii <101170415+dangeunii@users.noreply.github.com>
2024-10-11 13:25:18 +0900

    Title: 꼬리잡기놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241011/꼬리잡기놀이/tail-catch-play.py
+++ 241011/꼬리잡기놀이/tail-catch-play.py
@@ -9,7 +9,6 @@ dx = [0, 1, 0, -1]
 
 n, m, k = map(int, input().split())
 mapp = []
-m2= [[0]*n for _ in range(n)]
 
 answer = 0
 
@@ -18,10 +17,6 @@ for i in range(n):
     mapp.append(m_r)
 
 v = [[0]*n for _ in range(n)]
-
-for i in range(n):
-    for j in range(n):
-        m2[i][j] = mapp[i][j]
 
 # [1-1] team 구현하기
 # team은 bfs를 이용하며, 머리를 찾는 순간 시작된다.
@@ -58,7 +53,7 @@ for y in range(n):
             team_num += 1
 
 for turn in range(1,k+1):
-
+    #print("turn : ", turn)
     ## [1-2] 팀 내에서 이동 구현하기
     ## 꼬리부터 한칸씩 앞으로 이동 구현하기
     ## 꼬리는 pop, 머리는 머리앞의 4 insert
@@ -104,23 +99,45 @@ for turn in range(1,k+1):
                     ## 해당 팀에서 공을 맞으면 순서 뒤집기!!
                     teams[mapp[t][i]].reverse()
 
-                #print(point)
                 break
     elif n + 1 <= turn <= 2 * n:
         for i in range(n):
-            if mapp[n - i][k - n] != 4:
-                point = mapp[turn][i] ** 2
+            # 일단 만나는 점이 있는지 찾기
+            # team 번호
+            if mapp[n-i-1][turn-n-1] != 4 and mapp[n-i-1][turn-n-1] != 0:
+                #print(n-i-1, turn-n-1)
+                team = mapp[n-i-1][turn-n-1]
+
+                #print("team: " ,teams[team])
+
+                # 그리고 그 해당 좌표의 index 찾아서 point 구하기
+                if (n-i-1,turn-n-1) in teams[team]:
+                    idx = teams[team].index(((n-i-1),(turn-n-1)))
+                    #print(idx)
+                    point = (idx+1) **2
+                    ## 해당 팀의 머리 꼬리 변경
+                    teams[team].reverse()
+                #print("point: ", point)
                 break
     elif 2 * n + 1 <= turn <= 3 * n:
         for i in range(n):
-            if m[3 * n - turn][n - i] != 4:
-                point = mapp[turn][i] ** 2
-                break
+            if mapp[3*n - turn - 1][n-i-1] != 4 and mapp[3*n - turn - 1] [n-i-1] != 0:
+               team = mapp[3*n-turn -1][n-i-1]
+               if ((3*n - turn - 1),(n-i-1)) in teams[team]:
+                   idx = teams[team].index((3*n - turn - 1),(n-i-1))
+                   point = (idx+1) ** 2
+                   teams[team].reverse()
+            break
+
     elif 3 * n + 1 <= turn <= 4 * n:
         for i in range(n):
-            if m[i][4 * turn - n] != 4:
-                point = mapp[k][i] ** 2
-                break
+            if mapp[i][4*n -turn -1] != 4 and mapp[i][4*n -turn -1] != 0:
+                team = mapp[i][4*n - turn -1]
+                if (i, 4*n -turn-1) in teams[team]:
+                    idx = teams[team].index(i,4*n -turn-1)
+                    point = (idx+1) **2
+                    teams[team].reverse()
+            break
     answer += point
 
 print(answer)
