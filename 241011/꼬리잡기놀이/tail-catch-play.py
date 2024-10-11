26d41b632526bebaf8a32d58d0db1fe07eac4523
dangeunii <101170415+dangeunii@users.noreply.github.com>
2024-10-11 13:28:02 +0900

    Title: 꼬리잡기놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241011/꼬리잡기놀이/tail-catch-play.py
+++ 241011/꼬리잡기놀이/tail-catch-play.py
@@ -1,143 +1,118 @@
 from collections import deque
+## 구현해야 할 기능
+## [1] 예술성 점수 구하기 - bfs로
+## [1-1] pic 내에 들어있는 숫자 종류, 그룹별 좌표 구하기
+## [1-2] 예술성 = (a그룹 칸 수 + b 그룹 칸수) * 그룹 a 숫자 * 그룹 b 숫자 * a와 b가 맞닿아있는 변의 수(강의에서는 조합에서 서로 인접하다고 판정되면 이 앞까지의 점수를 누적해서 더하는 것으로 구함!)
+## [2] 십자모양 반시계 방향 90도 회전
+## [3] N//2 * N//2 배열 시계 방향 90도 회전
+## [4] 이 모든건 3번 반복 후 총합
 
-# 입력 구현
-# n : n개 줄, m : 팀의 갯수, k : 라운드 수
-# map에서 1 : 머리사람, 2: 중간 사람, 3: 꼬리 사람, 4 : 이동 경로 표시
+# 입력받는 값
+# n : n개의 줄
+# pic : 초기 그림(숫자 배열)
 
-dy = [-1, 0 , 1, 0]
+n = int(input())
+pic = []
+for row in range(n):
+    pic_r = list(map(int, input().split()))
+    pic.append(pic_r)
+
+dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
+m = n //2
 
-n, m, k = map(int, input().split())
-mapp = []
-
+## [1] 그룹을 계산하고 그룹별 예술성 구하기
+## 그룹을 구한다 -> dfs
+num = 0
+cnt = 0
+isVisited = [[0] * n for _ in range(n)]
 answer = 0
+n_pic = [[0] * n for _ in range(n)]
 
-for i in range(n):
-    m_r = list(map(int, input().split()))
-    mapp.append(m_r)
+def rotate_90(sy, sx, length):
+    global pic, n_pic
 
-v = [[0]*n for _ in range(n)]
+    for y in range(sy, sy+length):
+        for x in range(sx, sx+length):
+            # 1. (0,0)으로 평행이동
+            oy = y - sy
+            ox = x - sx
 
-# [1-1] team 구현하기
-# team은 bfs를 이용하며, 머리를 찾는 순간 시작된다.
-team_num = 5
-teams = {}
+            # 2. rotate
+            ry , rx = ox, length - oy -1
 
-def bfs(y,x,team_num):
-    team = []
+            # 3. 적용하기
+            n_pic[ry + sy][rx + sx] = pic[y][x]
+
+    for y in range(sy, sy+length):
+        for x in range(sx, sx + length):
+            pic[y][x] = n_pic[y][x]
+
+def bfs(y,x):
     q = deque()
     q.append((y,x))
-    v[y][x] = 1
-    ## team으로 좌표 추가
-    team.append((y,x))
-    mapp[y][x] = team_num
+    isVisited[y][x] = 1
+    groups[-1].add((y,x))
 
-    while(q):
-        (sy,sx) = q.popleft()
+    while q:
+        (cy,cx) = q.popleft()
         for i in range(4):
-            ny = sy + dy[i]
-            nx = sx + dx[i]
-            # 범위 내, 미방문, 2쪽 or 출발지에서 온게 아니라면 3도 가능
-            if 0<= ny < n and 0<= nx < n and v[ny][nx] == 0:
-                if mapp[ny][nx] == 2 or ((sy,sx) != (y,x) and mapp[ny][nx] == 3):
-                    q.append((ny,nx))
-                    team.append((ny,nx))
-                    mapp[ny][nx] = team_num
-    teams[team_num] = team
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0<= ny < n and 0 <= nx < n and isVisited[ny][nx] == 0 and pic[ny][nx] == pic[y][x]:
+                q.append((ny,nx))
+                isVisited[ny][nx] = 1
+                groups[-1].add((ny,nx))
 
 
-for y in range(n):
+for k in range(4):
+
+    for y in range(n):
+        for x in range(n):
+            isVisited[y][x] = 0
+
+    nums = []
+    groups = []
+
+    ## [1] 예술성 점수 구하기
+    ## [1-1] bfs를 통해서 group, num 구하기
     for x in range(n):
-        if mapp[y][x] == 1 and v[y][x] == 0:
-            bfs(y,x,team_num)
-            team_num += 1
+        for y in range(n):
+            if isVisited[y][x] == 0:
+                nums.append(pic[y][x])
+                groups.append(set())
+                bfs(y,x)
 
-for turn in range(1,k+1):
-    #print("turn : ", turn)
-    ## [1-2] 팀 내에서 이동 구현하기
-    ## 꼬리부터 한칸씩 앞으로 이동 구현하기
-    ## 꼬리는 pop, 머리는 머리앞의 4 insert
-    for team in teams.values():
-        ## 꼬리 1칸 앞으로
-        ey,ex = team.pop()
-        eny, enx = team[-1][0:]
-        mapp[eny][enx] = mapp[ey][ex]
-        mapp[ey][ex] = 4
-
-        ## 머리 1칸 앞으로
-        sy,sx = team[0][0:]
-        for i in range(4):
-            ny = sy + dy[i]
-            nx = sx + dx[i]
-            # 범위 내, 값이 4
-            if 0 <= ny <n and 0 <= nx < n and mapp[ny][nx] == 4:
-                team.insert(0,(ny,nx))
-                mapp[ny][nx] = mapp[sy][sx]
-
-    ##for i in range(n):
-    ##    print(mapp[i])
-
-    # [2] 공 쏘기
-    # k : 1 ~ n : (k,i)
-    # k : n+1 ~ 2n : (n-i,k-n)
-    # k : 2n+1 ~ 3n : (3n-k,n-i)
-    # k : 3n+1 ~ 4n : (i, 4n-k)
-    #print(teams)
+    #print(groups)
+    ## [1-2] 예술성 점수 = (a그룹 칸 수 + b 그룹 칸수) * 그룹 a 숫자 * 그룹 b 숫자 * a와 b가 맞닿아있는 변의 수(강의에서는 조합에서 서로 인접하다고 판정되면 이 앞까지의 점수를 누적해서 더하는 것으로 구함!)
+    cnt = len(nums)
     point = 0
-    if 1 <= turn <= n:
-        for i in range(n):
-            t = turn - 1
-            if mapp[t][i] != 4 and mapp[t][i] != 0:
-                ty , tx = t, i
-                #print(ty,tx)
-                #print(mapp[t][i])
-                if (ty,tx) in teams[mapp[t][i]]:
-                    idx = teams[mapp[t][i]].index((ty,tx))
-                    #print("idx : ",idx)
-                    point = (idx+1) **2
+    for i in range(0,cnt-1):
+        for j in range(i+1, cnt):
+            point = (len(groups[i])+len(groups[j])) * nums[i] * nums[j]
+            for (si, sj) in groups[i]:
+                for l in range(4):
+                    ni = si + dy[l]
+                    nj = sj + dx[l]
+                    if (ni, nj) in groups[j]:
+                        answer += point
+    #print("answer : ", answer)
+    if k == 3:
+        break
 
-                    ## 해당 팀에서 공을 맞으면 순서 뒤집기!!
-                    teams[mapp[t][i]].reverse()
+    #print(pic)
+    ## [2] 회전하기 - 3번의 회전
+    ## [2-1] 십자가 모양의 회전
+    for a in range(n):
+        n_pic[m][a] = pic[a][m]
+        n_pic[a][m] = pic[m][n-1-a]
 
-                break
-    elif n + 1 <= turn <= 2 * n:
-        for i in range(n):
-            # 일단 만나는 점이 있는지 찾기
-            # team 번호
-            if mapp[n-i-1][turn-n-1] != 4 and mapp[n-i-1][turn-n-1] != 0:
-                #print(n-i-1, turn-n-1)
-                team = mapp[n-i-1][turn-n-1]
-
-                #print("team: " ,teams[team])
-
-                # 그리고 그 해당 좌표의 index 찾아서 point 구하기
-                if (n-i-1,turn-n-1) in teams[team]:
-                    idx = teams[team].index(((n-i-1),(turn-n-1)))
-                    #print(idx)
-                    point = (idx+1) **2
-                    ## 해당 팀의 머리 꼬리 변경
-                    teams[team].reverse()
-                #print("point: ", point)
-                break
-    elif 2 * n + 1 <= turn <= 3 * n:
-        for i in range(n):
-            if mapp[3*n - turn - 1][n-i-1] != 4 and mapp[3*n - turn - 1] [n-i-1] != 0:
-               team = mapp[3*n-turn -1][n-i-1]
-               if ((3*n - turn - 1),(n-i-1)) in teams[team]:
-                   idx = teams[team].index((3*n - turn - 1),(n-i-1))
-                   point = (idx+1) ** 2
-                   teams[team].reverse()
-            break
-
-    elif 3 * n + 1 <= turn <= 4 * n:
-        for i in range(n):
-            if mapp[i][4*n -turn -1] != 4 and mapp[i][4*n -turn -1] != 0:
-                team = mapp[i][4*n - turn -1]
-                if (i, 4*n -turn-1) in teams[team]:
-                    idx = teams[team].index((i,4*n -turn-1))
-                    point = (idx+1) **2
-                    teams[team].reverse()
-            break
-    answer += point
+    rotate_90(0,0,m)
+    rotate_90(0,m+1, m)
+    rotate_90(m+1, 0, m)
+    rotate_90(m+1, m+1, m)
+    for i in range(n):
+        for j in range(n):
+            pic[i][j] = n_pic[i][j]
 
 print(answer)
